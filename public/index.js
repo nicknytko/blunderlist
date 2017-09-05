5a422cddec9b39268f3be53ebf25ba851263df58
Nicolas Nytko <nicknytko@gmail.com>
2017-09-05 09:04:19 -0500

    fix month value in datetime fields and add link to git

--- public/index.js
+++ public/index.js
@@ -18,7 +18,7 @@ function format_datetime( date )
     }
 
     return date.getFullYear( ) + '-' +
-	pad( date.getMonth( ) - 1 ) + '-' +
+	pad( date.getMonth( ) + 1 ) + '-' +
 	pad( date.getDate( ) ) + 'T' +
 	pad( date.getHours( ) ) + ':' +
 	pad( date.getMinutes( ) );
